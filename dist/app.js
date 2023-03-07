f45ec2500941d8d1505fab8043b669450e5ea057
Tahir iqbal <shahtahir786@gmail.com>
2023-03-07 13:56:19 +0530

    updated sidebar

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,14 @@
-//sidebar
-
+let openBtn = document.getElementById("open")
+let toggleImgBtn1 = document.getElementById("toggleImg1")
+let toggleImgBtn2 = document.getElementById("toggleImg2")
 let sidebarEl = document.getElementById("sidebar")
-let sidebarBtn = document.getElementById("sidebar-btn")
+let linkTextEl = document.querySelectorAll(".link-text")
 
-sidebarBtn.addEventListener("click", () => {
-    sidebarEl.classList.toggle("hidden")
-})
+for(let i=0; i<linkTextEl.length; i++){
+    openBtn.addEventListener("click", () => {
+        linkTextEl[i].classList.toggle("hidden")
+        sidebarEl.classList.toggle("w-[16rem]")
+        toggleImgBtn1.classList.toggle("hidden")
+        toggleImgBtn2.classList.toggle("hidden")
+    })
+}
